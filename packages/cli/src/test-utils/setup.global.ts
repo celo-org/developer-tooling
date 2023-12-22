ffa76c42adc55493a7dcfda0d280458f3ef37916
Aaron <aaron.deruvo@clabs.co>
2023-12-22 15:44:34 +0200

    did it fix it? no but these are help to have

--- packages/cli/src/test-utils/setup.global.ts
+++ packages/cli/src/test-utils/setup.global.ts
@@ -1,6 +1,5 @@
 import baseSetup from '@celo/dev-utils/lib/ganache-setup'
 // Has to import the matchers somewhere so that typescript knows the matchers have been made available
-import _unused from '@celo/dev-utils/lib/matchers'
 import * as path from 'path'
 
 // Warning: There should be an unused import of '@celo/dev-utils/lib/matchers' above.
@@ -13,6 +12,7 @@ export default async function globalSetup() {
   await baseSetup(path.resolve(chainDataPath), 'v10.tar.gz', {
     from_targz: true,
   })
+  console.log('\n ganache started...')
   // it is necessary to disabled oclif integration with ts-node as
   // together it leads to a silent signit error and exit when tsconfk is loaded.
   // @ts-ignore - because global this doesnt have oclif property
