747fd39ceac3069e92ff4fb8ae1258c688ac70e3
Aaron <aaron.deruvo@clabs.co>
2024-01-04 17:04:15 +0200

    tried and true

--- packages/cli/src/test-utils/setup.global.ts
+++ packages/cli/src/test-utils/setup.global.ts
@@ -15,5 +15,6 @@ export default async function globalSetup() {
   })
   // it is necessary to disabled oclif integration with ts-node as
   // together it leads to a silent signit error and exit when tsconfk is loaded.
+  // @ts-ignore - because global this doesnt have oclif property
   global.oclif = { tsnodeEnabled: false }
 }
