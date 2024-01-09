a79b1ff80f3a0a5558414081532422e49799d03b
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2024-01-09 17:14:46 +0100

    chore: remove releasegold

--- packages/cli/.eslintrc.js
+++ packages/cli/.eslintrc.js
@@ -1,6 +1,3 @@
 module.exports = {
   extends: '../../.eslintrc.js',
-  // the releasegold folder is a symlink of releasecelo
-  // can't add a .eslintignore since it'd ignore the src folder
-  ignorePatterns: ['**/commands/releasegold/*'],
 }
