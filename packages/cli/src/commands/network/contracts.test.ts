0e069866c8311dcbb6ad4f944f07ca61b9ea2c79
Aaron <aaron.deruvo@clabs.co>
2024-02-21 17:05:43 +0200

    fix #83 -- when a contract does not have an implementation safely move on, settings its version to zero

---
+++ packages/cli/src/commands/network/contracts.test.ts
@@ -0,0 +1,13 @@
+import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import write from '@oclif/core/lib/cli-ux/write'
+import { testLocally } from '../../test-utils/cliUtils'
+import Contracts from './contracts'
+process.env.NO_SYNCCHECK = 'true'
+
+testWithGanache('network:contracts', () => {
+  test('runs', async () => {
+    const spy = jest.spyOn(write, 'stdout')
+    await testLocally(Contracts, ['--output', 'json'])
+    expect(spy.mock.calls).toMatchSnapshot()
+  })
+})
