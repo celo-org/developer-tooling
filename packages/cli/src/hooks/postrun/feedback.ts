646d355f3c35a0af0e684d1c51908abfbd7c0cc2
Aaron <aaron.deruvo@clabs.co>
2024-02-22 13:40:50 +0200

    make question more general

--- packages/cli/src/hooks/postrun/feedback.ts
+++ packages/cli/src/hooks/postrun/feedback.ts
@@ -1,10 +1,10 @@
 import { Hook, ux } from '@oclif/core'
 
-const hook: Hook<'postrun'> = async function (options) {
-  ux.info(`Thanks for using the celocli!
-We want to know which commands are most useful to the community.
-Is '${options.Command.id}' a command you use often?
-Let us know at https://github.com/celo-org/developer-tooling/discussions/92
+const hook: Hook<'postrun'> = async function () {
+  ux.info(`
+  Thanks for using the celocli! We are interested in your feedback.
+  Let us know at https://github.com/celo-org/developer-tooling/discussions/92
+
 `)
 }
 
