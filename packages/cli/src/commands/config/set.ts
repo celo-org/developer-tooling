87a402c2ba8762152f31b618bce811abecf3283f
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-03-01 00:02:20 -0300

    chore(cli): adds TODOs and updates example

--- packages/cli/src/commands/config/set.ts
+++ packages/cli/src/commands/config/set.ts
@@ -18,8 +18,7 @@ export default class Set extends BaseCommand {
   static examples = [
     'set --node ws://localhost:2500',
     'set --node <geth-location>/geth.ipc',
-    'set --gasCurrency cUSD',
-    'set --gasCurrency CELO',
+    'set --gasCurrency 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
   ]
 
   requireSynced = false
@@ -28,10 +27,14 @@ export default class Set extends BaseCommand {
     const res = await this.parse(Set)
     const curr = readConfig(this.config.configDir)
     const node = res.flags.node ?? curr.node
-    // TODO(Arthur): Update `gasCurrency` logic here
+    // TODO(Arthur): Check `gasCurrency` logic works as expected here
     const gasCurrency = res.flags.gasCurrency
       ? (gasOptions as any)[res.flags.gasCurrency as string]
       : curr.gasCurrency
+    /**
+     * TODO(Arthur): Consider if this is a good place to implement a runtime check that
+     * asserts the `gasCurrency` argument is a valid fee currency, before writing config.
+     */
     writeConfig(this.config.configDir, {
       node,
       gasCurrency,
