8ceef091d2907c4848eead0f14eff244e50455af
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2024-03-20 12:14:48 +0100

    test: test adapted tokens too

--- packages/cli/src/commands/network/whitelist.test.ts
+++ packages/cli/src/commands/network/whitelist.test.ts
@@ -1,3 +1,4 @@
+import { FeeCurrencyWhitelistWrapper } from '@celo/contractkit/lib/wrappers/FeeCurrencyWhitelistWrapper'
 import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
 import { testLocally } from '../../test-utils/cliUtils'
 import Whitelist from './whitelist'
@@ -7,10 +8,14 @@ process.env.NO_SYNCCHECK = 'true'
 // Lots of commands, sometimes times out
 jest.setTimeout(15000)
 
+const spy = jest.spyOn(console, 'log')
+
+beforeEach(() => {
+  spy.mockClear()
+})
+
 testWithGanache('network:whitelist cmd', () => {
   test('can print the whitelist', async () => {
-    const spy = jest.spyOn(console, 'log')
-
     await testLocally(Whitelist, [])
     expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
       "Available currencies:
@@ -18,5 +23,29 @@ testWithGanache('network:whitelist cmd', () => {
       0x965D352283a3C8A016b9BBbC9bf6306665d495E7 - Celo Brazilian Real (cREAL)
       0xdD66C23e07b4D6925b6089b5Fe6fc9E62941aFE8 - Celo Euro (cEUR)"
     `)
+  })
+
+  test('handles adapted tokens too', async () => {
+    const mock = jest
+      .spyOn(FeeCurrencyWhitelistWrapper.prototype, 'getFeeCurrencyInformation')
+      .mockImplementation(() =>
+        Promise.resolve([
+          {
+            name: 'mock token',
+            symbol: 'MCK',
+            address: '0x123',
+            adaptedToken: '0x456',
+          },
+        ])
+      )
+
+    await testLocally(Whitelist, [])
+
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      "Available currencies:
+      0x123 - mock token (MCK) (adapted token: 0x456)"
+    `)
+
+    mock.mockClear()
   })
 })
