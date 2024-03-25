d8eb2e385e341f5bbc36dfb4f9ec5a1374ab308a
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2024-03-25 15:03:10 +0100

    feat: more examples

--- packages/cli/src/commands/config/set.ts
+++ packages/cli/src/commands/config/set.ts
@@ -13,7 +13,16 @@ export default class Set extends BaseCommand {
     },
   }
 
-  static examples = ['set --node ws://localhost:2500', 'set --node <geth-location>/geth.ipc']
+  static examples = [
+    'set --node mainnet # alias for `forno`',
+    'set --node forno # alias for https://forno.celo.org',
+    'set --node baklava # alias for https://baklava-forno.celo-testnet.org',
+    'set --node alfajores # alias for https://alfajores-forno.celo-testnet.org',
+    'set --node localhost # alias for `local`',
+    'set --node local # alias for http://localhost:8545',
+    'set --node ws://localhost:2500',
+    'set --node <geth-location>/geth.ipc',
+  ]
 
   requireSynced = false
 
