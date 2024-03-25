880a8386e10cf9cd84c57d141330e84635a7eeaa
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2024-03-25 15:53:07 +0100

    feat: ignores gasCurrency when passed to the config (#198)
    * feat: ignores gasCurrency when passed to the config
    
    * chore: add changeset
    
    * fix: PR feedback
    
    * fix: PR feedback
    
    * feat: more examples
    
    * fix: bad refactor
    
    * test: test readConfig

--- .changeset/khaki-lies-wait.md
+++
@@ -1,9 +0,0 @@
----
-'@celo/celocli': major
----
-
-The following changes are related to adding support for more fee currencies in the @celo packages.
-
-(BREAKING): `--gasCurrency` changed to accept only whitelisted addresses or the string `CELO` instead of previously accepting a StableToken or 'auto'
-(ADDED): `celocli network:whitelist` prints the whitelisted feeCurrencies
-(ADDED): the cli will automagically convert the previous gasCurrency such as cEUR, cUSD, cREAL, CELO into its address if necessary
--- packages/cli/src/utils/config.ts
+++ packages/cli/src/utils/config.ts
@@ -1,20 +1,15 @@
-import { StableToken, StrongAddress, Token } from '@celo/base'
-import { ContractKit } from '@celo/contractkit'
-import { isValidAddress } from '@celo/utils/lib/address'
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
 export interface CeloConfig {
   node: string
-  gasCurrency?: StrongAddress
 }
 
-// This interface handles compatibility with previous version of the CLI where
-// gas currency was passed differently
-export interface LegacyCeloConfig {
-  node: string
-  gasCurrency?: StableToken | Token | 'auto'
-}
+// NOTE: this mapping should stay updated as CeloConfig evolves
+const LEGACY_MAPPING: Record<string, keyof CeloConfig | undefined> = {
+  nodeUrl: 'node',
+  gasCurrency: undefined,
+} as const
 
 export const defaultConfig: CeloConfig = {
   node: 'http://localhost:8545',
@@ -30,9 +25,15 @@ export function readConfig(configDir: string): CeloConfig {
   if (fs.pathExistsSync(configPath(configDir))) {
     const existingConfig = fs.readJSONSync(configPath(configDir))
     const combinedConfig = { ...defaultConfig, ...existingConfig }
-    if (combinedConfig.hasOwnProperty('nodeUrl')) {
-      combinedConfig.node = combinedConfig.nodeUrl
+
+    // NOTE: make sure we don't confuse the user by printing legacy config elements
+    for (const [legacyKey, newKey] of Object.entries(LEGACY_MAPPING)) {
+      if (newKey && combinedConfig[legacyKey]) {
+        combinedConfig[newKey] = combinedConfig[legacyKey]
+      }
+      delete combinedConfig[legacyKey]
     }
+
     return combinedConfig
   } else {
     return defaultConfig
@@ -43,71 +44,8 @@ export function getNodeUrl(configDir: string): string {
   return readConfig(configDir).node
 }
 
-export async function getGasCurrency(
-  configDir: string,
-  kit: ContractKit
-): Promise<StrongAddress | undefined> {
-  const config = readConfig(configDir) as CeloConfig | LegacyCeloConfig
-  const { gasCurrency } = config
-
-  if (!gasCurrency) return
-  if (isValidAddress(gasCurrency)) return gasCurrency
-
-  // Force rewriting the config with Token->Address conversion
-  await writeConfig(configDir, config, kit)
-
-  return getGasCurrency(configDir, kit)
-}
-
-export async function writeConfig(
-  configDir: string,
-  configObj: CeloConfig | LegacyCeloConfig,
-  kit: ContractKit
-) {
+export async function writeConfig(configDir: string, configObj: CeloConfig) {
   const config = { ...defaultConfig, ...configObj }
-  const { gasCurrency } = config
-
-  if (gasCurrency) {
-    if (gasCurrency.startsWith('0x')) {
-      if (isValidAddress(gasCurrency)) {
-        const feeCurrencyWhitelist = await kit.contracts.getFeeCurrencyWhitelist()
-        const validFeeCurrencies = await feeCurrencyWhitelist.getWhitelist()
-
-        if (!validFeeCurrencies.includes(gasCurrency)) {
-          const pairs = (
-            await feeCurrencyWhitelist.getFeeCurrencyInformation(validFeeCurrencies)
-          ).map(
-            ({ name, symbol, address, adaptedToken }) =>
-              `${address} - ${name || 'unknown name'} (${symbol || 'N/A'})${
-                adaptedToken ? ` (adapted token: ${adaptedToken})` : ''
-              }`
-          )
-          throw new Error(
-            `${config.gasCurrency} is not a valid fee currency. Available currencies:\n${pairs.join(
-              '\n'
-            )}`
-          )
-        }
-      } else {
-        throw new Error(`Invalid address ${gasCurrency}`)
-      }
-    } else {
-      // NOTE: This handles a legacy version of celocli where the gasCurrency
-      // was passed as cEUR, cUSD, or cREAL
-      switch (gasCurrency) {
-        case 'auto':
-        case Token.CELO:
-          delete config.gasCurrency
-          break
-        case StableToken.cEUR:
-        case StableToken.cUSD:
-        case StableToken.cREAL:
-          const { address } = await kit.contracts.getStableToken(gasCurrency as StableToken)
-          ;(config as CeloConfig).gasCurrency = address
-          break
-      }
-    }
-  }
 
   fs.outputJSONSync(configPath(configDir), config)
 }
