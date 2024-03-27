be2ed7078b413aa73239cd90be5b681e890274eb
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-03-27 11:58:16 +0300

    try mocking the calls to network

--- packages/cli/src/test-utils/setup.ts
+++ packages/cli/src/test-utils/setup.ts
@@ -1,3 +1,9 @@
+const fetchMockSandbox = require('fetch-mock').sandbox()
+jest.mock('cross-fetch', () => fetchMockSandbox)
+
+// @ts-ignore
+global.fetchMock = fetchMockSandbox
+
 jest.mock('@ledgerhq/hw-transport-node-hid', () => {
   return {
     default: {
