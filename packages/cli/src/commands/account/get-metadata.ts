11f6e0acdf764bee86fde352cc185f51f10284e7
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-04-25 13:46:37 +0100

    chore(cli): adds back `any` type mistakenly deleted by linter
    Mistakenly deleted by the linter in this commit 7ad20991e6864f9e38025757b5d82eec2b65a9bf when saving.
    
    Quick fix was to disable `formatOnSave` in `.vscode/settings.json`:
    
    ```diff
    "[typescript]": {
    +    "editor.formatOnSave": false,
    -    "editor.formatOnSave": true,
    ```

--- packages/cli/src/commands/account/get-metadata.ts
+++ packages/cli/src/commands/account/get-metadata.ts
@@ -39,7 +39,7 @@ export default class GetMetadata extends BaseCommand {
       )
       console.info('Metadata contains the following claims: \n')
       await displayMetadata(metadata, kit, flags)
-    } catch (error) {
+    } catch (error: any) {
       console.error(`Metadata could not be retrieved from ${metadataURL}: ${error.toString()}`)
     }
   }
