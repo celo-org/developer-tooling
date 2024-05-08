98289d786caa8d372b2bc51c44372be2655991f9
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-05-08 12:07:38 +0200

    claryfing comment

--- packages/sdk/connect/src/utils/is-cel2.ts
+++ packages/sdk/connect/src/utils/is-cel2.ts
@@ -1,5 +1,10 @@
 import Web3 from 'web3'
 
+/*
+ * This util checks if we're in L2 context, it's a port of the technique used in
+ * https://github.com/celo-org/celo-monorepo/blob/da9b4955c1fdc8631980dc4adf9b05e0524fc228/packages/protocol/contracts-0.8/common/IsL2Check.sol#L17
+ */
+
 export const PROXY_ADMIN_ADDRESS = '0x4200000000000000000000000000000000000018'
 
 export const isCel2 = async (web3: Web3) => {
