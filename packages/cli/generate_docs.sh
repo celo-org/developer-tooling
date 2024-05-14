3cb9349ec9ce3c081df8be622ef8d4523707c116
Arthur Gousset <46296830+arthurgousset@users.noreply.github.com>
2024-05-14 11:19:01 +0100

    docs(cli/generate_docs): remove code comment
    Co-authored-by: Leszek Stachowski <leszek.stachowski@clabs.co>

--- packages/cli/generate_docs.sh
+++ packages/cli/generate_docs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 export COLUMNS=88
-# must replace lib with src so docs link to source code
 yarn oclif readme --multi --dir=../docs/command-line-interface
 sed -i.bak '/^- \[/d' ../docs/command-line-interface/*
 rm -rf ../docs/command-line-interface/*.bak
