ceac069d65600243fb137693a783a22355ddd6b2
Arthur Gousset <46296830+arthurgousset@users.noreply.github.com>
2024-05-14 11:19:08 +0100

    docs(cli/generate_docs): remove code comment
    Co-authored-by: Leszek Stachowski <leszek.stachowski@clabs.co>

--- packages/cli/generate_docs.sh
+++ packages/cli/generate_docs.sh
@@ -4,4 +4,3 @@ export COLUMNS=88
 yarn oclif readme --multi --dir=../docs/command-line-interface
 sed -i.bak '/^- \[/d' ../docs/command-line-interface/*
 rm -rf ../docs/command-line-interface/*.bak
-# reverse changes to package.json
