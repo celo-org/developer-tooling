ff3a3d2129b48714df7d6b2555bac0acc429052a
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-06-05 12:24:50 +0200

    add await to setupL2 test

--- packages/cli/src/test-utils/chain-setup.test.ts
+++ packages/cli/src/test-utils/chain-setup.test.ts
@@ -1,19 +1,19 @@
-import { isCel2 } from "@celo/connect";
-import { testWithAnvil } from "@celo/dev-utils/lib/anvil-test";
-import Web3 from "web3";
-import { setupL2 } from "./chain-setup";
-import { newKitFromWeb3 } from "@celo/contractkit";
+import { isCel2 } from '@celo/connect'
+import { newKitFromWeb3 } from '@celo/contractkit'
+import { testWithAnvil } from '@celo/dev-utils/lib/anvil-test'
+import Web3 from 'web3'
+import { setupL2 } from './chain-setup'
 
-testWithAnvil("chain setup", (web3: Web3) => {
-  describe("setupL2()", () => {
-    it("sets up L2 context", async () => {
-      const kit = newKitFromWeb3(web3);
+testWithAnvil('chain setup', (web3: Web3) => {
+  describe('setupL2()', () => {
+    it('sets up L2 context', async () => {
+      const kit = newKitFromWeb3(web3)
 
-      expect(await isCel2(web3)).toEqual(false);
+      expect(await isCel2(web3)).toEqual(false)
 
-      setupL2(kit);
+      await setupL2(kit)
 
-      expect(await isCel2(web3)).toEqual(true);
-    });
-  });
-});
+      expect(await isCel2(web3)).toEqual(true)
+    })
+  })
+})
