9d47b219979ec3cfdb60110450a6164941276578
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-06-06 16:43:54 +0200

    refactor test to not expect failure

--- packages/sdk/contractkit/src/wrappers/FeeCurrencyDirectoryWrapper.test.ts
+++ packages/sdk/contractkit/src/wrappers/FeeCurrencyDirectoryWrapper.test.ts
@@ -1,13 +1,33 @@
-import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import { testWithAnvil } from '@celo/dev-utils/lib/anvil-test'
 import { newKitFromWeb3 } from '../kit'
 
-testWithGanache('FeeCurrencyDirectory', (web3) => {
+testWithAnvil('FeeCurrencyDirectory', (web3) => {
   const kit = newKitFromWeb3(web3)
 
-  // Fails with "FeeCurrencyDirectory not (yet) registered"
-  it.failing('fetches fee currency information', async () => {
+  it('fetches fee currency information', async () => {
     const wrapper = await kit.contracts.getFeeCurrencyDirectory()
 
-    expect(wrapper.getFeeCurrencyInformation()).toMatchInlineSnapshot()
+    expect(await wrapper.getFeeCurrencyInformation()).toMatchInlineSnapshot(`
+      [
+        {
+          "adaptedToken": undefined,
+          "address": "0x2A3733dBc31980f02b12135C809b5da33BF3a1e9",
+          "name": "Celo Brazilian Real",
+          "symbol": "Celo Brazilian Real",
+        },
+        {
+          "adaptedToken": undefined,
+          "address": "0xb7a33b4ad2B1f6b0a944232F5c71798d27Ad9272",
+          "name": "Celo Euro",
+          "symbol": "Celo Euro",
+        },
+        {
+          "adaptedToken": undefined,
+          "address": "0xe6774BE4E5f97dB10cAFB4c00C74cFbdCDc434D9",
+          "name": "Celo Dollar",
+          "symbol": "Celo Dollar",
+        },
+      ]
+    `)
   })
 })
