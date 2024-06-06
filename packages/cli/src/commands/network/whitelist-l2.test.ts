aa58119f8cb6839a2644197768c8d03c145e220b
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-06-06 16:22:21 +0200

    remove mocks for whitelist-l2 test

--- packages/cli/src/commands/network/whitelist-l2.test.ts
+++ packages/cli/src/commands/network/whitelist-l2.test.ts
@@ -1,18 +1,9 @@
-import * as celoConnect from '@celo/connect'
 import { newKitFromWeb3 } from '@celo/contractkit'
-import { WrapperCache } from '@celo/contractkit/lib/contract-cache'
-import { FeeCurrencyDirectoryWrapper } from '@celo/contractkit/lib/wrappers/FeeCurrencyDirectoryWrapper'
-import { testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import { testWithAnvil } from '@celo/dev-utils/lib/anvil-test'
 import Web3 from 'web3'
-import { testLocally } from '../../test-utils/cliUtils'
+import { setupL2 } from '../../test-utils/chain-setup'
+import { testLocallyWithWeb3Node } from '../../test-utils/cliUtils'
 import Whitelist from './whitelist'
-
-jest.mock('@celo/connect', () => {
-  return {
-    __esModule: true,
-    ...jest.requireActual('@celo/connect'),
-  }
-})
 
 process.env.NO_SYNCCHECK = 'true'
 
@@ -22,27 +13,19 @@ afterAll(() => {
   jest.clearAllMocks()
 })
 
-testWithGanache('network:whitelist cmd', (web3: Web3) => {
+testWithAnvil('network:whitelist cmd', (web3: Web3) => {
   test('can print the whitelist', async () => {
+    // @ts-ignore
     const kit = newKitFromWeb3(web3)
-    const isCel2Mock = jest.spyOn(celoConnect, 'isCel2')
-    const getFeeCurrencyDirectoryMock = jest.spyOn(
-      WrapperCache.prototype,
-      'getFeeCurrencyDirectory'
-    )
 
-    // TODO remove proxying and mocking when FeeCurrencyDirectory is available
-    isCel2Mock.mockResolvedValue(true)
-    getFeeCurrencyDirectoryMock.mockImplementation(async () => {
-      return kit.contracts.getFeeCurrencyWhitelist() as unknown as FeeCurrencyDirectoryWrapper
-    })
+    await setupL2(kit)
 
-    await testLocally(Whitelist, [])
+    await testLocallyWithWeb3Node(Whitelist, [], web3)
     expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
       "Available currencies:
-      0x5315e44798395d4a952530d131249fE00f554565 - Celo Dollar (cUSD)
-      0x965D352283a3C8A016b9BBbC9bf6306665d495E7 - Celo Brazilian Real (cREAL)
-      0xdD66C23e07b4D6925b6089b5Fe6fc9E62941aFE8 - Celo Euro (cEUR)"
+      0x2A3733dBc31980f02b12135C809b5da33BF3a1e9 - Celo Brazilian Real (Celo Brazilian Real)
+      0xb7a33b4ad2B1f6b0a944232F5c71798d27Ad9272 - Celo Euro (Celo Euro)
+      0xe6774BE4E5f97dB10cAFB4c00C74cFbdCDc434D9 - Celo Dollar (Celo Dollar)"
     `)
   })
 })
