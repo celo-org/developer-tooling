938b3d9ff9c5e4a4bbe17c08b21049d80de5f29f
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-06-07 14:55:21 +0200

    kit not required

--- packages/cli/src/test-utils/chain-setup.test.ts
+++ packages/cli/src/test-utils/chain-setup.test.ts
@@ -1,5 +1,4 @@
 import { isCel2 } from '@celo/connect'
-import { newKitFromWeb3 } from '@celo/contractkit'
 import { testWithAnvil } from '@celo/dev-utils/lib/anvil-test'
 import Web3 from 'web3'
 import { setupL2 } from './chain-setup'
@@ -7,11 +6,9 @@ import { setupL2 } from './chain-setup'
 testWithAnvil('chain setup', (web3: Web3) => {
   describe('setupL2()', () => {
     it('sets up L2 context', async () => {
-      const kit = newKitFromWeb3(web3)
-
       expect(await isCel2(web3)).toEqual(false)
 
-      await setupL2(kit.web3)
+      await setupL2(web3)
 
       expect(await isCel2(web3)).toEqual(true)
     })
