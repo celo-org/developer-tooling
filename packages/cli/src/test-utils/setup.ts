7d0524033677e64897b1a658de32f22aaff5ad75
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-06-12 18:17:10 +0300

    oops wrong file

--- packages/cli/src/test-utils/setup.ts
+++ packages/cli/src/test-utils/setup.ts
@@ -1,5 +1,10 @@
 const fetchMockSandbox = require('fetch-mock').sandbox()
-jest.mock('cross-fetch', () => fetchMockSandbox)
+jest.mock('cross-fetch', () => ({
+  ...jest.requireActual('cross-fetch'),
+  __esModule: true,
+  default: fetchMockSandbox,
+  fetch: fetchMockSandbox,
+}))
 
 // @ts-ignore
 global.fetchMock = fetchMockSandbox
