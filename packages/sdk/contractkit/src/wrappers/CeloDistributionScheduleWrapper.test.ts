ac557b708b176084d3a0d50a1d3669f818fd9a58
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-06-26 17:12:20 +0200

    fix CeloDistributionScheduleWrapper.test.ts

--- packages/sdk/contractkit/src/wrappers/CeloDistributionScheduleWrapper.test.ts
+++ packages/sdk/contractkit/src/wrappers/CeloDistributionScheduleWrapper.test.ts
@@ -1,14 +1,5 @@
-import { setNextBlockTimestamp, setupL2, testWithAnvil } from '@celo/dev-utils/lib/anvil-test'
-import { TEST_TIMESTAMP } from '@celo/dev-utils/lib/test-utils'
+import { setupL2, testWithAnvil } from '@celo/dev-utils/lib/anvil-test'
 import { newKitFromWeb3 } from '../kit'
-
-// we need a fixed number, because otherwise different values than saved in the
-// snapshot are produced  as they depend on time
-//
-// in case the TEST_TIMESTAMP value gets updated and the test fails with
-// "<timestamp> is lower than or equal to previous block's timestamp"
-// then this value needs to be updated and so does the snapshot
-const NEXT_BLOCK_TIMESTAMP = TEST_TIMESTAMP + 60 * 60 * 24
 
 testWithAnvil('CeloDistributionScheduleWrapper', (web3) => {
   const kit = newKitFromWeb3(web3)
@@ -17,7 +8,6 @@ testWithAnvil('CeloDistributionScheduleWrapper', (web3) => {
     const celoDistributionScheduleWrapper = await kit.contracts.getCeloDistributionSchedule()
 
     await setupL2(web3)
-    await setNextBlockTimestamp(web3, NEXT_BLOCK_TIMESTAMP.toString())
 
     const config = await celoDistributionScheduleWrapper.getConfig()
     expect(config).toMatchInlineSnapshot(`
