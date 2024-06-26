e250f6993eba2ab91a6e0b8a82800329c3c966de
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-06-26 17:09:02 +0200

    fix network:info snapshot

--- packages/cli/src/commands/network/info-l2.test.ts
+++ packages/cli/src/commands/network/info-l2.test.ts
@@ -13,7 +13,7 @@ testWithAnvil('network:info', (web3) => {
     expect(stripAnsiCodesFromNestedArray(spy.mock.calls)).toMatchInlineSnapshot(`
       [
         [
-          "blockNumber: 269",
+          "blockNumber: 270",
         ],
       ]
     `)
