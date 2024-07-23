2cdb00fa2632314dc04a67571404dc746994fe4e
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-07-23 13:14:35 +0200

    @celo/contractkit: migrate Governance wrapper tests to anvil

--- packages/dev-utils/src/chain-setup.ts
+++ packages/dev-utils/src/chain-setup.ts
@@ -1,3 +1,4 @@
+import { newGovernance } from '@celo/abis/web3/Governance'
 import { newValidators } from '@celo/abis/web3/Validators'
 import { StrongAddress } from '@celo/base'
 import Web3 from 'web3'
@@ -12,6 +13,34 @@ export async function setCommissionUpdateDelay(
     const validators = newValidators(web3, validatorsContractAddress)
 
     await validators.methods.setCommissionUpdateDelay(delayInBlocks).send({
+      from: DEFAULT_OWNER_ADDRESS,
+    })
+  })
+}
+
+export async function setDequeueFrequency(
+  web3: Web3,
+  governanceContractAddress: StrongAddress,
+  frequency: number
+) {
+  withImpersonatedAccount(web3, DEFAULT_OWNER_ADDRESS, async () => {
+    const governance = newGovernance(web3, governanceContractAddress)
+
+    await governance.methods.setDequeueFrequency(frequency).send({
+      from: DEFAULT_OWNER_ADDRESS,
+    })
+  })
+}
+
+export async function setReferendumStageDuration(
+  web3: Web3,
+  governanceContractAddress: StrongAddress,
+  duration: number
+) {
+  withImpersonatedAccount(web3, DEFAULT_OWNER_ADDRESS, async () => {
+    const governance = newGovernance(web3, governanceContractAddress)
+
+    await governance.methods.setReferendumStageDuration(duration).send({
       from: DEFAULT_OWNER_ADDRESS,
     })
   })
