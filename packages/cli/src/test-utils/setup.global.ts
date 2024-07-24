f3824bf2491d01f6c97a49f1a88b6fa415ac7327
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-07-24 12:24:10 +0200

    restore needed line

--- packages/cli/src/test-utils/setup.global.ts
+++ packages/cli/src/test-utils/setup.global.ts
@@ -1,6 +1,7 @@
 import baseSetup from '@celo/dev-utils/lib/ganache-setup'
 import { shouldRunGanacheTests } from '@celo/dev-utils/lib/ganache-test'
 // Has to import the matchers somewhere so that typescript knows the matchers have been made available
+import _unused from '@celo/dev-utils/lib/matchers'
 import * as path from 'path'
 
 // Warning: There should be an unused import of '@celo/dev-utils/lib/matchers' above.
