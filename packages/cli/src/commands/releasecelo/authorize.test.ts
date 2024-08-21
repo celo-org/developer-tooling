a3f9a888e3f9ea9dd7680084721d8e35558aede9
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-08-21 13:43:36 +0200

    Migrate releasecelo command tests to anvil (#331)

--- packages/cli/src/commands/releasecelo/authorize.test.ts
+++ packages/cli/src/commands/releasecelo/authorize.test.ts
@@ -1,8 +1,12 @@
+import { NULL_ADDRESS, StrongAddress } from '@celo/base'
 import { newKitFromWeb3 } from '@celo/contractkit'
-import { getContractFromEvent, testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import { setBalance, testWithAnvilL1 } from '@celo/dev-utils/lib/anvil-test'
 import { addressToPublicKey, serializeSignature } from '@celo/utils/lib/signatureUtils'
+import BigNumber from 'bignumber.js'
 import Web3 from 'web3'
-import { testLocally } from '../../test-utils/cliUtils'
+import { testLocallyWithWeb3Node } from '../../test-utils/cliUtils'
+import { createMultisig } from '../../test-utils/multisigUtils'
+import { deployReleaseGoldContract } from '../../test-utils/release-gold'
 import ValidatorRegister from '../validator/register'
 import Authorize from './authorize'
 import CreateAccount from './create-account'
@@ -10,17 +14,30 @@ import LockedGold from './locked-gold'
 
 process.env.NO_SYNCCHECK = 'true'
 
-testWithGanache('releasegold:authorize cmd', (web3: Web3) => {
+testWithAnvilL1('releasegold:authorize cmd', (web3: Web3) => {
   let contractAddress: string
   let kit: any
 
   beforeEach(async () => {
-    contractAddress = await getContractFromEvent(
-      'ReleaseGoldInstanceCreated(address,address)',
-      web3
-    )
+    const accounts = (await web3.eth.getAccounts()) as StrongAddress[]
     kit = newKitFromWeb3(web3)
-    await testLocally(CreateAccount, ['--contract', contractAddress])
+    contractAddress = await deployReleaseGoldContract(
+      web3,
+      await createMultisig(kit, [accounts[0], accounts[1]] as StrongAddress[], 2, 2),
+      accounts[1],
+      accounts[0],
+      NULL_ADDRESS, // when cannot be revoked and can validate then refund address needs to be NULL_ADDRESS
+      true // can validate
+    )
+
+    // contract needs to have sufficient funds to lock CELO
+    await setBalance(
+      web3,
+      contractAddress as StrongAddress,
+      new BigNumber(web3.utils.toWei('100000', 'ether'))
+    )
+
+    await testLocallyWithWeb3Node(CreateAccount, ['--contract', contractAddress], web3)
   })
 
   describe('can authorize account signers', () => {
@@ -34,42 +51,54 @@ testWithGanache('releasegold:authorize cmd', (web3: Web3) => {
     })
 
     test('can authorize account vote signer ', async () => {
-      await testLocally(Authorize, [
-        '--contract',
-        contractAddress,
-        '--role',
-        'vote',
-        '--signer',
-        accounts[1],
-        '--signature',
-        serializeSignature(pop),
-      ])
+      await testLocallyWithWeb3Node(
+        Authorize,
+        [
+          '--contract',
+          contractAddress,
+          '--role',
+          'vote',
+          '--signer',
+          accounts[1],
+          '--signature',
+          serializeSignature(pop),
+        ],
+        web3
+      )
     })
 
     test('can authorize account validator signer', async () => {
-      await testLocally(Authorize, [
-        '--contract',
-        contractAddress,
-        '--role',
-        'validator',
-        '--signer',
-        accounts[1],
-        '--signature',
-        serializeSignature(pop),
-      ])
+      await testLocallyWithWeb3Node(
+        Authorize,
+        [
+          '--contract',
+          contractAddress,
+          '--role',
+          'validator',
+          '--signer',
+          accounts[1],
+          '--signature',
+          serializeSignature(pop),
+        ],
+        web3
+      )
     })
 
     test('can authorize account attestation signer', async () => {
-      await testLocally(Authorize, [
-        '--contract',
-        contractAddress,
-        '--role',
-        'attestation',
-        '--signer',
-        accounts[1],
-        '--signature',
-        serializeSignature(pop),
-      ])
+      await testLocallyWithWeb3Node(
+        Authorize,
+        [
+          '--contract',
+          contractAddress,
+          '--role',
+          'attestation',
+          '--signer',
+          accounts[1],
+          '--signature',
+          serializeSignature(pop),
+        ],
+        web3
+      )
     })
   })
 
@@ -79,36 +108,48 @@ testWithGanache('releasegold:authorize cmd', (web3: Web3) => {
     const signer = accounts[1]
     const pop = await accountsWrapper.generateProofOfKeyPossession(contractAddress, signer)
     const ecdsaPublicKey = await addressToPublicKey(signer, web3.eth.sign)
-    await testLocally(LockedGold, [
-      '--contract',
-      contractAddress,
-      '--action',
-      'lock',
-      '--value',
-      '10000000000000000000000',
-      '--yes',
-    ])
-    await testLocally(Authorize, [
-      '--contract',
-      contractAddress,
-      '--role',
-      'validator',
-      '--signer',
-      signer,
-      '--signature',
-      serializeSignature(pop),
-    ])
-    await testLocally(ValidatorRegister, [
-      '--from',
-      signer,
-      '--ecdsaKey',
-      ecdsaPublicKey,
-      '--blsKey',
-      '0x4fa3f67fc913878b068d1fa1cdddc54913d3bf988dbe5a36a20fa888f20d4894c408a6773f3d7bde11154f2a3076b700d345a42fd25a0e5e83f4db5586ac7979ac2053cd95d8f2efd3e959571ceccaa743e02cf4be3f5d7aaddb0b06fc9aff00',
-      '--blsSignature',
-      '0xcdb77255037eb68897cd487fdd85388cbda448f617f874449d4b11588b0b7ad8ddc20d9bb450b513bb35664ea3923900',
-      '--yes',
-    ])
+    await testLocallyWithWeb3Node(
+      LockedGold,
+      [
+        '--contract',
+        contractAddress,
+        '--action',
+        'lock',
+        '--value',
+        '10000000000000000000000',
+        '--yes',
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      Authorize,
+      [
+        '--contract',
+        contractAddress,
+        '--role',
+        'validator',
+        '--signer',
+        signer,
+        '--signature',
+        serializeSignature(pop),
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      ValidatorRegister,
+      [
+        '--from',
+        signer,
+        '--ecdsaKey',
+        ecdsaPublicKey,
+        '--blsKey',
+        '0x4fa3f67fc913878b068d1fa1cdddc54913d3bf988dbe5a36a20fa888f20d4894c408a6773f3d7bde11154f2a3076b700d345a42fd25a0e5e83f4db5586ac7979ac2053cd95d8f2efd3e959571ceccaa743e02cf4be3f5d7aaddb0b06fc9aff00',
+        '--blsSignature',
+        '0xcdb77255037eb68897cd487fdd85388cbda448f617f874449d4b11588b0b7ad8ddc20d9bb450b513bb35664ea3923900',
+        '--yes',
+      ],
+      web3
+    )
   })
 
   test('can authorize signer with BLS keys after registering as validator', async () => {
@@ -123,50 +164,66 @@ testWithGanache('releasegold:authorize cmd', (web3: Web3) => {
     const newBlsPublicKey = web3.utils.randomHex(96)
     const newBlsPoP = web3.utils.randomHex(48)
 
-    await testLocally(LockedGold, [
-      '--contract',
-      contractAddress,
-      '--action',
-      'lock',
-      '--value',
-      '10000000000000000000000',
-      '--yes',
-    ])
-    await testLocally(Authorize, [
-      '--contract',
-      contractAddress,
-      '--role',
-      'validator',
-      '--signer',
-      signer,
-      '--signature',
-      serializeSignature(pop),
-    ])
-    await testLocally(ValidatorRegister, [
-      '--from',
-      signer,
-      '--ecdsaKey',
-      ecdsaPublicKey,
-      '--blsKey',
-      '0x4fa3f67fc913878b068d1fa1cdddc54913d3bf988dbe5a36a20fa888f20d4894c408a6773f3d7bde11154f2a3076b700d345a42fd25a0e5e83f4db5586ac7979ac2053cd95d8f2efd3e959571ceccaa743e02cf4be3f5d7aaddb0b06fc9aff00',
-      '--blsSignature',
-      '0xcdb77255037eb68897cd487fdd85388cbda448f617f874449d4b11588b0b7ad8ddc20d9bb450b513bb35664ea3923900',
-      '--yes',
-    ])
-    await testLocally(Authorize, [
-      '--contract',
-      contractAddress,
-      '--role',
-      'validator',
-      '--signer',
-      signerBLS,
-      '--signature',
-      serializeSignature(popBLS),
-      '--blsKey',
-      newBlsPublicKey,
-      '--blsPop',
-      newBlsPoP,
-    ])
+    await testLocallyWithWeb3Node(
+      LockedGold,
+      [
+        '--contract',
+        contractAddress,
+        '--action',
+        'lock',
+        '--value',
+        '10000000000000000000000',
+        '--yes',
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      Authorize,
+      [
+        '--contract',
+        contractAddress,
+        '--role',
+        'validator',
+        '--signer',
+        signer,
+        '--signature',
+        serializeSignature(pop),
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      ValidatorRegister,
+      [
+        '--from',
+        signer,
+        '--ecdsaKey',
+        ecdsaPublicKey,
+        '--blsKey',
+        '0x4fa3f67fc913878b068d1fa1cdddc54913d3bf988dbe5a36a20fa888f20d4894c408a6773f3d7bde11154f2a3076b700d345a42fd25a0e5e83f4db5586ac7979ac2053cd95d8f2efd3e959571ceccaa743e02cf4be3f5d7aaddb0b06fc9aff00',
+        '--blsSignature',
+        '0xcdb77255037eb68897cd487fdd85388cbda448f617f874449d4b11588b0b7ad8ddc20d9bb450b513bb35664ea3923900',
+        '--yes',
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      Authorize,
+      [
+        '--contract',
+        contractAddress,
+        '--role',
+        'validator',
+        '--signer',
+        signerBLS,
+        '--signature',
+        serializeSignature(popBLS),
+        '--blsKey',
+        newBlsPublicKey,
+        '--blsPop',
+        newBlsPoP,
+      ],
+      web3
+    )
   })
 
   test('cannot authorize signer without BLS keys after registering as validator', async () => {
@@ -179,63 +236,83 @@ testWithGanache('releasegold:authorize cmd', (web3: Web3) => {
     const signerNew = accounts[2]
     const popNew = await accountsWrapper.generateProofOfKeyPossession(contractAddress, signerNew)
 
-    await testLocally(LockedGold, [
-      '--contract',
-      contractAddress,
-      '--action',
-      'lock',
-      '--value',
-      '10000000000000000000000',
-      '--yes',
-    ])
-    await testLocally(Authorize, [
-      '--contract',
-      contractAddress,
-      '--role',
-      'validator',
-      '--signer',
-      signer,
-      '--signature',
-      serializeSignature(pop),
-    ])
-    await testLocally(ValidatorRegister, [
-      '--from',
-      signer,
-      '--ecdsaKey',
-      ecdsaPublicKey,
-      '--blsKey',
-      '0x4fa3f67fc913878b068d1fa1cdddc54913d3bf988dbe5a36a20fa888f20d4894c408a6773f3d7bde11154f2a3076b700d345a42fd25a0e5e83f4db5586ac7979ac2053cd95d8f2efd3e959571ceccaa743e02cf4be3f5d7aaddb0b06fc9aff00',
-      '--blsSignature',
-      '0xcdb77255037eb68897cd487fdd85388cbda448f617f874449d4b11588b0b7ad8ddc20d9bb450b513bb35664ea3923900',
-      '--yes',
-    ])
-    await expect(
-      testLocally(Authorize, [
+    await testLocallyWithWeb3Node(
+      LockedGold,
+      [
+        '--contract',
+        contractAddress,
+        '--action',
+        'lock',
+        '--value',
+        '10000000000000000000000',
+        '--yes',
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      Authorize,
+      [
         '--contract',
         contractAddress,
         '--role',
         'validator',
         '--signer',
-        signerNew,
+        signer,
         '--signature',
-        serializeSignature(popNew),
-      ])
+        serializeSignature(pop),
+      ],
+      web3
+    )
+    await testLocallyWithWeb3Node(
+      ValidatorRegister,
+      [
+        '--from',
+        signer,
+        '--ecdsaKey',
+        ecdsaPublicKey,
+        '--blsKey',
+        '0x4fa3f67fc913878b068d1fa1cdddc54913d3bf988dbe5a36a20fa888f20d4894c408a6773f3d7bde11154f2a3076b700d345a42fd25a0e5e83f4db5586ac7979ac2053cd95d8f2efd3e959571ceccaa743e02cf4be3f5d7aaddb0b06fc9aff00',
+        '--blsSignature',
+        '0xcdb77255037eb68897cd487fdd85388cbda448f617f874449d4b11588b0b7ad8ddc20d9bb450b513bb35664ea3923900',
+        '--yes',
+      ],
+      web3
+    )
+    await expect(
+      testLocallyWithWeb3Node(
+        Authorize,
+        [
+          '--contract',
+          contractAddress,
+          '--role',
+          'validator',
+          '--signer',
+          signerNew,
+          '--signature',
+          serializeSignature(popNew),
+        ],
+        web3
+      )
     ).rejects.toThrow()
   })
 
   test('fails if contract is not registered as an account', async () => {
     const accounts = await web3.eth.getAccounts()
     await expect(
-      testLocally(Authorize, [
-        '--contract',
-        contractAddress,
-        '--role',
-        'validator',
-        '--signer',
-        accounts[1],
-        '--signature',
-        '0x1b9fca4bbb5bfb1dbe69ef1cddbd9b4202dcb6b134c5170611e1e36ecfa468d7b46c85328d504934fce6c2a1571603a50ae224d2b32685e84d4d1a1eebad8452eb',
-      ])
+      testLocallyWithWeb3Node(
+        Authorize,
+        [
+          '--contract',
+          contractAddress,
+          '--role',
+          'validator',
+          '--signer',
+          accounts[1],
+          '--signature',
+          '0x1b9fca4bbb5bfb1dbe69ef1cddbd9b4202dcb6b134c5170611e1e36ecfa468d7b46c85328d504934fce6c2a1571603a50ae224d2b32685e84d4d1a1eebad8452eb',
+        ],
+        web3
+      )
     ).rejects.toThrow()
   })
 })
--- packages/cli/src/commands/releasecelo/refund-and-finalize.test.ts
+++ packages/cli/src/commands/releasecelo/refund-and-finalize.test.ts
@@ -1,30 +1,37 @@
 import { newReleaseGold } from '@celo/abis/web3/ReleaseGold'
+import { StrongAddress } from '@celo/base'
 import { ContractKit, newKitFromWeb3 } from '@celo/contractkit'
 import { ReleaseGoldWrapper } from '@celo/contractkit/lib/wrappers/ReleaseGold'
-import { getContractFromEvent, testWithGanache } from '@celo/dev-utils/lib/ganache-test'
+import { testWithAnvilL1 } from '@celo/dev-utils/lib/anvil-test'
+import { getContractFromEvent } from '@celo/dev-utils/lib/ganache-test'
 import Web3 from 'web3'
-import { testLocally } from '../../test-utils/cliUtils'
+import { testLocallyWithWeb3Node } from '../../test-utils/cliUtils'
+import { createMultisig } from '../../test-utils/multisigUtils'
+import { deployReleaseGoldContract } from '../../test-utils/release-gold'
 import RefundAndFinalize from './refund-and-finalize'
 import Revoke from './revoke'
-import Show from './show'
 
 process.env.NO_SYNCCHECK = 'true'
 
-testWithGanache('releasegold:refund-and-finalize cmd', (web3: Web3) => {
+testWithAnvilL1('releasegold:refund-and-finalize cmd', (web3: Web3) => {
   let contractAddress: any
   let kit: ContractKit
 
   beforeEach(async () => {
-    contractAddress = await getContractFromEvent(
-      'ReleaseGoldInstanceCreated(address,address)',
-      web3,
-      { index: 1 } // revocable = true
-    )
+    const accounts = (await web3.eth.getAccounts()) as StrongAddress[]
     kit = newKitFromWeb3(web3)
+
+    contractAddress = await deployReleaseGoldContract(
+      web3,
+      await createMultisig(kit, [accounts[0], accounts[1]] as StrongAddress[], 2, 2),
+      accounts[1],
+      accounts[0],
+      accounts[2]
+    )
   })
 
   test('can refund celo', async () => {
-    await testLocally(Revoke, ['--contract', contractAddress, '--yesreally'])
+    await testLocallyWithWeb3Node(Revoke, ['--contract', contractAddress, '--yesreally'], web3)
     const releaseGoldWrapper = new ReleaseGoldWrapper(
       kit.connection,
       newReleaseGold(web3, contractAddress),
@@ -32,14 +39,34 @@ testWithGanache('releasegold:refund-and-finalize cmd', (web3: Web3) => {
     )
     const refundAddress = await releaseGoldWrapper.getRefundAddress()
     const balanceBefore = await kit.getTotalBalance(refundAddress)
-    await testLocally(RefundAndFinalize, ['--contract', contractAddress])
+    await testLocallyWithWeb3Node(RefundAndFinalize, ['--contract', contractAddress], web3)
     const balanceAfter = await kit.getTotalBalance(refundAddress)
     expect(balanceBefore.CELO!.toNumber()).toBeLessThan(balanceAfter.CELO!.toNumber())
   })
 
   test('can finalize the contract', async () => {
-    await testLocally(Revoke, ['--contract', contractAddress, '--yesreally'])
-    await testLocally(RefundAndFinalize, ['--contract', contractAddress])
-    await expect(testLocally(Show, ['--contract', contractAddress])).rejects.toThrow()
+    const releaseGoldWrapper = new ReleaseGoldWrapper(
+      kit.connection,
+      newReleaseGold(web3, contractAddress),
+      kit.contracts
+    )
+
+    expect(await releaseGoldWrapper.isRevoked()).toBe(false)
+    await testLocallyWithWeb3Node(Revoke, ['--contract', contractAddress, '--yesreally'], web3)
+    expect(await releaseGoldWrapper.isRevoked()).toBe(true)
+
+    // Contract still should have some balance
+    expect((await kit.getTotalBalance(contractAddress)).CELO).not.toEqBigNumber(0)
+
+    await testLocallyWithWeb3Node(RefundAndFinalize, ['--contract', contractAddress], web3)
+
+    const destroyedContractAddress = await getContractFromEvent(
+      'ReleaseGoldInstanceDestroyed(address,address)',
+      web3
+    )
+
+    expect(destroyedContractAddress).toBe(contractAddress)
+    // Contract should not have any balance left anymore
+    expect((await kit.getTotalBalance(contractAddress)).CELO).toEqBigNumber(0)
   })
 })
