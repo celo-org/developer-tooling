2e6875e42b624f55e2899a7a18f1cf06336b3440
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-09-07 15:15:53 +0200

    fix it so that when values are printed the order of numbers is numeric

---
+++ packages/cli/src/utils/cli.test.ts
@@ -0,0 +1,61 @@
+import { testWithAnvilL2 } from '@celo/dev-utils/lib/anvil-test'
+import { stripAnsiCodesFromNestedArray } from '../test-utils/cliUtils'
+import { printValueMapRecursive } from './cli'
+testWithAnvilL2('printValueMapRecursive', async () => {
+  it('should print the key-value pairs in the value map recursively', () => {
+    const valueMap = {
+      key1: 'value1',
+      key2: {
+        nestedKey1: 'nestedValue1',
+        nestedKey2: 'nestedValue2',
+      },
+    }
+
+    const mock = jest.fn()
+    console.log = mock
+
+    printValueMapRecursive(valueMap)
+
+    expect(stripAnsiCodesFromNestedArray(mock.mock.calls)).toMatchInlineSnapshot(``)
+  })
+
+  it('should print number keys in numeric order', () => {
+    const valueMap = {
+      1: 'jeden',
+      2: {
+        1: '1',
+        2: '2',
+        11: '3',
+        12: '4',
+        21: '5',
+      },
+      3: {
+        '0x1': 'nestedValue1',
+        jupiter: 'nestedValue2',
+        potato: 'nestedValue3',
+      },
+    }
+
+    const mock = jest.fn()
+    console.log = mock
+
+    printValueMapRecursive(valueMap)
+
+    expect(stripAnsiCodesFromNestedArray(mock.mock.calls)).toMatchInlineSnapshot(`
+      [
+        [
+          "1: jeden
+      2: 
+        1: 1
+        2: 2
+        11: 3
+        12: 4
+        21: 5
+      3: 
+        0x1: nestedValue1
+        jupiter: nestedValue2",
+        ],
+      ]
+    `)
+  })
+})
