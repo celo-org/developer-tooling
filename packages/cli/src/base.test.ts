567b2a6711d9aa2f88f91c4356bade3a826b07c3
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-09-11 13:10:47 +0200

    bump up coverage

--- packages/cli/src/base.test.ts
+++ packages/cli/src/base.test.ts
@@ -1,18 +1,20 @@
+import { Connection } from '@celo/connect'
 import { testWithAnvilL1 } from '@celo/dev-utils/lib/anvil-test'
+import { ux } from '@oclif/core'
 import Web3 from 'web3'
 import { BaseCommand } from './base'
 import { testLocallyWithWeb3Node } from './test-utils/cliUtils'
 
 process.env.NO_SYNCCHECK = 'true'
 
-class TestErrorCommand extends BaseCommand {
-  async run() {
-    throw new Error('test error')
-  }
-}
-
 testWithAnvilL1('BaseCommand', (web3: Web3) => {
   it('logs command execution error', async () => {
+    class TestErrorCommand extends BaseCommand {
+      async run() {
+        throw new Error('test error')
+      }
+    }
+
     const errorSpy = jest.spyOn(console, 'error')
 
     await expect(testLocallyWithWeb3Node(TestErrorCommand, [], web3)).rejects.toThrow()
@@ -27,6 +29,41 @@ testWithAnvilL1('BaseCommand', (web3: Web3) => {
 
       ",
           [Error: test error],
+        ],
+      ]
+    `)
+  })
+
+  it('logs connection close error', async () => {
+    class TestConnectionStopErrorCommand extends BaseCommand {
+      async run() {
+        console.log('Successful run')
+      }
+    }
+
+    const writeMock = jest.spyOn(ux.write, 'stdout')
+    const logSpy = jest.spyOn(console, 'log')
+    const errorSpy = jest.spyOn(console, 'error')
+
+    jest.spyOn(Connection.prototype, 'stop').mockImplementation(() => {
+      throw new Error('Mock connection stop error')
+    })
+
+    await testLocallyWithWeb3Node(TestConnectionStopErrorCommand, [], web3)
+
+    expect(logSpy.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          "Successful run",
+        ],
+      ]
+    `)
+    expect(errorSpy.mock.calls).toMatchInlineSnapshot(`[]`)
+    expect(writeMock.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          "Failed to close the connection: Error: Mock connection stop error
+      ",
         ],
       ]
     `)
