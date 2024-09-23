1bc24ae16fb3bdb73449bb8c30aa231a176f24d5
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-09-23 13:15:05 +0200

    fininsh epochs:switch command test

--- packages/cli/src/commands/epochs/switch.test.ts
+++ packages/cli/src/commands/epochs/switch.test.ts
@@ -1,14 +1,27 @@
 import { newKitFromWeb3 } from '@celo/contractkit'
 import { testWithAnvilL2 } from '@celo/dev-utils/lib/anvil-test'
+import { timeTravel } from '@celo/dev-utils/lib/ganache-test'
+import BigNumber from 'bignumber.js'
 import { testLocallyWithWeb3Node } from '../../test-utils/cliUtils'
 import Switch from './switch'
+
+process.env.NO_SYNCCHECK = 'true'
 
 testWithAnvilL2('epochs:switch cmd', (web3) => {
   it('switches epoch successfully', async () => {
     const kit = newKitFromWeb3(web3)
     const accounts = await kit.web3.eth.getAccounts()
+    const epochManagerWrapper = await kit.contracts.getEpochManager()
+    const epochDuration = new BigNumber(await epochManagerWrapper.epochDuration())
 
-    // TODO doesn't work for now
+    await timeTravel(epochDuration.plus(1).toNumber(), web3)
+
+    expect(await epochManagerWrapper.getCurrentEpochNumber()).toEqual('4')
+    expect(await epochManagerWrapper.isTimeForNextEpoch()).toEqual(true)
+
     await testLocallyWithWeb3Node(Switch, ['--from', accounts[0]], web3)
+
+    expect(await epochManagerWrapper.getCurrentEpochNumber()).toEqual('5')
+    expect(await epochManagerWrapper.isTimeForNextEpoch()).toEqual(false)
   })
 })
