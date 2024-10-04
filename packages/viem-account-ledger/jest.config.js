809e0587f96e8484e70fad415b45d23dc520361e
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2024-10-04 10:39:15 +0200

    chore: coverage ignore a couple files

--- packages/viem-account-ledger/jest.config.js
+++ packages/viem-account-ledger/jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).ts?(x)'],
   setupFilesAfterEnv: ['@celo/dev-utils/lib/matchers'],
   verbose: true,
+  coveragePathIgnorePatterns: [
+    // NOTE: both the following files are copy pasted from other repos
+    // will add tests as/if we refactor them
+    'src/tokens.ts',
+    'src/data.ts',
+  ],
 }
