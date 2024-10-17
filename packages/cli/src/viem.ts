178d6b4e60253d342698c49fd3aca27e8751fd16
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-10-17 17:50:09 +0200

    simplify client creation

--- packages/cli/src/viem.ts
+++ packages/cli/src/viem.ts
@@ -1,27 +1,16 @@
 import { Command } from '@oclif/core'
-import { createPublicClient, extractChain, http, HttpTransport, PublicClient } from 'viem'
-import { celo, celoAlfajores } from 'viem/chains'
+import { createPublicClient, http, HttpTransport, PublicClient } from 'viem'
+import { celo } from 'viem/chains'
 
 export abstract class ViemCommand extends Command {
   private publicClient?: PublicClient<HttpTransport, typeof celo>
 
   protected async getPublicClient(): Promise<PublicClient<HttpTransport, typeof celo>> {
     if (!this.publicClient) {
-      // TEMP
-      const nodeUrl = 'http://127.0.0.1:8545'
-      const transport = http(nodeUrl)
       // Create an intermediate client to get the chain id
-      const intermediateClient = createPublicClient({
-        transport,
-      })
-      const extractedChain = extractChain({
-        chains: [celo, celoAlfajores],
-        id: (await intermediateClient.getChainId()) as 42220 | 44787,
-      })
-
       this.publicClient = createPublicClient({
-        transport,
-        chain: extractedChain ?? celo,
+        transport: http(),
+        chain: celo,
       }) as PublicClient<HttpTransport, typeof celo>
     }
 
