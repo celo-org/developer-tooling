4e24087650fe080a50df787623a0caaa3ee62855
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-10-17 18:05:41 +0200

    remove typing

--- packages/cli/src/viem.ts
+++ packages/cli/src/viem.ts
@@ -1,17 +1,17 @@
 import { Command } from '@oclif/core'
-import { createPublicClient, http, HttpTransport, PublicClient } from 'viem'
+import { createPublicClient, http } from 'viem'
 import { celo } from 'viem/chains'
 
 export abstract class ViemCommand extends Command {
-  private publicClient?: PublicClient<HttpTransport, typeof celo>
+  // @ts-ignore debug
+  private publicClient
 
-  protected async getPublicClient(): Promise<PublicClient<HttpTransport, typeof celo>> {
+  protected async getPublicClient() {
     if (!this.publicClient) {
-      // Create an intermediate client to get the chain id
       this.publicClient = createPublicClient({
         transport: http(),
         chain: celo,
-      }) as PublicClient<HttpTransport, typeof celo>
+      })
     }
 
     return this.publicClient
