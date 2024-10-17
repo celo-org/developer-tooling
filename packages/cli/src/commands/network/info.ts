14366a172ef9cb3857cbb8dfdade848061e53bef
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-10-17 15:46:02 +0200

    break tests

--- packages/cli/src/commands/network/info.ts
+++ packages/cli/src/commands/network/info.ts
@@ -1,7 +1,8 @@
 import { Flags } from '@oclif/core'
 import { BaseCommand } from '../../base'
+import { ViemCommand } from '../../viem'
 
-export default class Info extends BaseCommand {
+export default class Info extends ViemCommand {
   static description = 'View general network information such as the current block number'
 
   static flags = {
