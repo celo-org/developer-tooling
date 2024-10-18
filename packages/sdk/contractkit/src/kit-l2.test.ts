dfaf7821f6a250572e824d5ffba0b973754c965e
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-10-18 15:03:44 +0200

    WIP

--- packages/sdk/contractkit/src/kit-l2.test.ts
+++ packages/sdk/contractkit/src/kit-l2.test.ts
@@ -1,7 +1,9 @@
 import { StrongAddress } from '@celo/base'
 import { testWithAnvilL2 } from '@celo/dev-utils/lib/anvil-test'
+import { timeTravel } from '@celo/dev-utils/lib/ganache-test'
 import Web3 from 'web3'
 import { ContractKit, newKitFromWeb3 } from './kit'
+import { startAndFinishEpochProcess } from './test-utils/utils'
 
 testWithAnvilL2('kit', (web3: Web3) => {
   let kit: ContractKit
@@ -66,6 +68,67 @@ testWithAnvilL2('kit', (web3: Web3) => {
           }
         `)
       })
+    })
+  })
+
+  describe('epochs', () => {
+    let epochDuration: number
+
+    beforeEach(async () => {
+      const epochManagerWrapper = await kit.contracts.getEpochManager()
+      epochDuration = await epochManagerWrapper.epochDuration()
+
+      // Go 3 epochs ahead
+      for (let i = 0; i < 3; i++) {
+        await timeTravel(epochDuration * 2, web3)
+        await startAndFinishEpochProcess(kit)
+      }
+
+      await timeTravel(epochDuration * 2, web3)
+
+      const accounts = await kit.web3.eth.getAccounts()
+
+      await epochManagerWrapper.startNextEpochProcess().sendAndWaitForReceipt({
+        from: accounts[0],
+      })
+
+      await (
+        await epochManagerWrapper.finishNextEpochProcessTx()
+      ).sendAndWaitForReceipt({
+        from: accounts[0],
+      })
+    })
+
+    it('gets the current epoch size', async () => {
+      expect(await kit.getEpochSize()).toEqual(epochDuration)
+    })
+
+    it('gets first and last block number of an epoch', async () => {
+      expect(await kit.getFirstBlockNumberForEpoch(4)).toEqual(300)
+      expect(await kit.getLastBlockNumberForEpoch(4)).toEqual(352)
+
+      expect(await kit.getFirstBlockNumberForEpoch(5)).toEqual(353)
+      expect(await kit.getLastBlockNumberForEpoch(5)).toEqual(355)
+
+      expect(await kit.getFirstBlockNumberForEpoch(6)).toEqual(356)
+      expect(await kit.getLastBlockNumberForEpoch(6)).toEqual(358)
+
+      expect(await kit.getFirstBlockNumberForEpoch(7)).toEqual(359)
+      expect(await kit.getLastBlockNumberForEpoch(7)).toEqual(371)
+
+      expect(await kit.getFirstBlockNumberForEpoch(8)).toEqual(372)
+    })
+
+    it('gets the current epoch number', async () => {
+      expect(await kit.getEpochNumberOfBlock(300)).toEqual(4)
+      expect(await kit.getEpochNumberOfBlock(357)).toEqual(6)
+      expect(await kit.getEpochNumberOfBlock(371)).toEqual(7)
+      expect(await kit.getEpochNumberOfBlock(372)).toEqual(8)
+    })
+
+    it('throws when block number is out of range', async () => {
+      await expect(kit.getEpochNumberOfBlock(299)).rejects.toThrow()
+      await expect(kit.getEpochNumberOfBlock(373)).rejects.toThrow()
     })
   })
 })
