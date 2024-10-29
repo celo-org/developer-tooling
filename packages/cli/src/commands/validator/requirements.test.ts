8ba1cdf8521a432d063a0f30bb46eac2bff655d4
Aaron DeRuvo <aaron.deruvo@clabs.co>
2024-10-29 12:16:45 +0100

    more validator tests

---
+++ packages/cli/src/commands/validator/requirements.test.ts
@@ -0,0 +1,26 @@
+import { testWithAnvilL2 } from '@celo/dev-utils/lib/anvil-test'
+import Web3 from 'web3'
+import { stripAnsiCodesFromNestedArray, testLocallyWithWeb3Node } from '../../test-utils/cliUtils'
+import Requirements from './requirements'
+
+process.env.NO_SYNCCHECK = 'true'
+
+testWithAnvilL2('validator:requirements', (web3: Web3) => {
+  const logMock = jest.spyOn(console, 'log')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('shows all registered validators', async () => {
+    await testLocallyWithWeb3Node(Requirements, [], web3)
+    expect(stripAnsiCodesFromNestedArray(logMock.mock.calls)).toMatchInlineSnapshot(`
+      [
+        [
+          "value: 1e+22
+      duration: 5184000",
+        ],
+      ]
+    `)
+  })
+})
