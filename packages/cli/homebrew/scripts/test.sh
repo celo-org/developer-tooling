e07ddafff9c7bf40690b5a5d55b29a44c01f2c72
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-03-06 12:50:35 +0100

    homebrew

--- packages/cli/homebrew/scripts/test.sh
+++ packages/cli/homebrew/scripts/test.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
+__dirname=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 brew update
-brew install ../Formula/celocli.rb
+brew install "$__dirname/../Formula/celocli.rb"
 celocli version
 celocli help
--- packages/cli/homebrew/templates/celocli.rb
+++ packages/cli/homebrew/templates/celocli.rb
@@ -29,26 +29,26 @@ class CeloCli < Formula
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/celocli"
 
-    bash_completion.install libexec/"autocomplete-scripts/brew/bash" => "celocli"
-    zsh_completion.install libexec/"autocomplete-scripts/brew/zsh/_celocli"
+    # bash_completion.install libexec/"autocomplete-scripts/brew/bash" => "celocli"
+    # zsh_completion.install libexec/"autocomplete-scripts/brew/zsh/_celocli"
   end
 
-  def caveats; <<~EOS
-    To use the Celo CLI's autocomplete --
-      Via homebrew's shell completion:
-        1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-            NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-                  and called, either explicitly or via a framework like oh-my-zsh.
-        2) Then run
-          $ celocli autocomplete --refresh-cache
-      OR
-      Use our standalone setup:
-        1) Run and follow the install steps:
-          $ celocli autocomplete
-  EOS
-  end
+  # def caveats; <<~EOS
+  #   To use the Celo CLI's autocomplete --
+  #     Via homebrew's shell completion:
+  #       1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
+  #           NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
+  #                 and called, either explicitly or via a framework like oh-my-zsh.
+  #       2) Then run
+  #         $ celocli autocomplete --refresh-cache
+  #     OR
+  #     Use our standalone setup:
+  #       1) Run and follow the install steps:
+  #         $ celocli autocomplete
+  # EOS
+  # end
 
   test do
-    system bin/"celocli", "version"
+    system bin/"celocli", "--version"
   end
 end
