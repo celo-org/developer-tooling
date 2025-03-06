ffdb836301bb483258027668a1e3a94d00b24b73
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-03-06 16:55:54 +0100

    fix homebrew test

--- packages/cli/homebrew/scripts/test.sh
+++ packages/cli/homebrew/scripts/test.sh
@@ -6,5 +6,5 @@ __dirname=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 brew update
 brew install "$__dirname/../Formula/celocli.rb"
-celocli version
+celocli --version
 celocli help
