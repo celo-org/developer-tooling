cb44ae63c01bb0e2579608ebb2ef9ce24dc579f0
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-03-17 17:14:53 +0100

    fix: remove unused caveats

--- packages/cli/homebrew/templates/celocli.rb
+++ packages/cli/homebrew/templates/celocli.rb
@@ -28,25 +28,7 @@ class Celocli < Formula
     inreplace "bin/celocli", /^CLIENT_HOME=/, "export CELOCLI_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/celocli"
-
-    # bash_completion.install libexec/"autocomplete-scripts/brew/bash" => "celocli"
-    # zsh_completion.install libexec/"autocomplete-scripts/brew/zsh/_celocli"
   end
-
-  # def caveats; <<~EOS
-  #   To use the Celo CLI's autocomplete --
-  #     Via homebrew's shell completion:
-  #       1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-  #           NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-  #                 and called, either explicitly or via a framework like oh-my-zsh.
-  #       2) Then run
-  #         $ celocli autocomplete --refresh-cache
-  #     OR
-  #     Use our standalone setup:
-  #       1) Run and follow the install steps:
-  #         $ celocli autocomplete
-  # EOS
-  # end
 
   test do
     system bin/"celocli", "--version"
