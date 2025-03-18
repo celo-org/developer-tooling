6cb946ed94cae12e9e5953bd817596ecc3fb3163
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-03-18 11:35:22 +0100

    homebrew checklist

--- packages/cli/homebrew/scripts/test.sh
+++ packages/cli/homebrew/scripts/test.sh
@@ -6,5 +6,6 @@ __dirname=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 brew update
 brew install "$__dirname/../Formula/celocli.rb"
+brew test celocli
 celocli --version
 celocli help
