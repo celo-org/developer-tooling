1580289673d2b128f6b4896acd8a353e90ca482c
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-03-18 11:16:29 +0100

    cleaup script and readme

---
+++ packages/cli/homebrew/scripts/cleanup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+arr=(
+  "darwin-arm64"
+  "darwin-x64"
+  "linux-arm"
+  "linux-arm64"
+  "linux-x64"
+  "win32-arm64"
+  "win32-x64"
+  "win32-x86"
+)
+sha=$1
+version=$2
+
+if [ -z "$sha" ]; then
+  echo "\`\$sha\` is empty"
+  echo "exiting…"
+  exit 1
+fi
+if [ -z "$version" ]; then
+  echo "\`\$version\` is empty"
+  echo "exiting…"
+  exit 1
+fi
+
+release="@celo/celocli@$version"
+file_prefix="celocli-v$version-$sha"
+
+while true; do
+  read -p "Do you wish to delete assets in $release matching $sha?" yn
+  case $yn in
+      [Yy]* ) echo "deleting assets…"; break;;
+      [Nn]* ) echo "exiting…"; exit;;
+      * ) echo "Please answer yes or no.";;
+  esac
+done
+
+for distro in "${arr[@]}";
+do
+  gh release delete-asset \
+    "$release" \
+    "$file_prefix-$distro.tar.xz" \
+    -y
+done
