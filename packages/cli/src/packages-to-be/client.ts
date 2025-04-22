8d2babcb58bbae87fa816cc3d58bf1c5e6671a8e
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-04-22 16:21:30 +0200

    fix: types

--- packages/cli/src/packages-to-be/client.ts
+++ packages/cli/src/packages-to-be/client.ts
@@ -1,8 +1,9 @@
 import { Account, PublicClient, Transport } from 'viem'
 import { celo, celoAlfajores } from 'viem/chains'
+import { celoBaklava } from './chains'
 
 export type CeloClient = PublicClient<
   Transport,
-  typeof celo | typeof celoAlfajores,
+  typeof celo | typeof celoAlfajores | typeof celoBaklava,
   Account | undefined
 >
