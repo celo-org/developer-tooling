9618f7e3dc7938193e2c693b24e238660d632c09
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-04-24 09:27:07 +0200

    fix: esm

--- packages/viem-account-ledger/scripts/post-build.mjs
+++ packages/viem-account-ledger/scripts/post-build.mjs
@@ -5,8 +5,7 @@ import { fileURLToPath } from 'node:url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const ESM_DIST = path.resolve(__dirname, '..', 'lib-es')
-const CJS_DIST = path.resolve(__dirname, '..', 'lib')
+const DIST = path.resolve(__dirname, '..', 'dist')
 
-writeFileSync(path.join(ESM_DIST, 'package.json'), JSON.stringify({ type: 'module' }, null, 4))
-writeFileSync(path.join(CJS_DIST, 'package.json'), JSON.stringify({ type: 'commonjs' }, null, 4))
+writeFileSync(path.join(DIST, 'mjs', 'package.json'), JSON.stringify({ type: 'module' }, null, 4))
+writeFileSync(path.join(DIST, 'cjs', 'package.json'), JSON.stringify({ type: 'commonjs' }, null, 4))
