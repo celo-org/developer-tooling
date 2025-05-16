677beb7664f8062f04ada1d12e760d373a846eb5
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-05-16 10:34:08 +0200

    reverse to do in other pr

--- packages/cli/src/commands/election/vote.ts
+++ packages/cli/src/commands/election/vote.ts
@@ -1,9 +1,10 @@
-import { getElectionContract } from '@celo/actions/contracts/election'
-import { WalletClient } from 'viem'
+import { Flags } from '@oclif/core'
+import BigNumber from 'bignumber.js'
 import { BaseCommand } from '../../base'
 import { newCheckBuilder } from '../../utils/checks'
-import { displaySendViemContractCall } from '../../utils/cli'
+import { displaySendTx } from '../../utils/cli'
 import { CustomFlags } from '../../utils/command'
+
 export default class ElectionVote extends BaseCommand {
   static description = 'Vote for a Validator Group in validator elections.'
 
@@ -14,28 +15,25 @@ export default class ElectionVote extends BaseCommand {
       description: "ValidatorGroup's address",
       required: true,
     }),
-    value: CustomFlags.bigint({
-      description: 'Amount of CELO used to vote for group',
-      required: true,
-    }),
+    value: Flags.string({ description: 'Amount of Gold used to vote for group', required: true }),
   }
 
   static examples = [
     'vote --from 0x4443d0349e8b3075cba511a0a87796597602a0f1 --for 0x932fee04521f5fcb21949041bf161917da3f588b, --value 1000000',
   ]
   async run() {
+    const kit = await this.getKit()
     const res = await this.parse(ElectionVote)
-    const value = res.flags.value
-
-    const client = await this.getWalletClient()
+    const value = new BigNumber(res.flags.value)
 
     await newCheckBuilder(this, res.flags.from)
       .isSignerOrAccount()
       .isValidatorGroup(res.flags.for)
-      .hasEnoughNonvotingLockedCelo(value)
+      .hasEnoughNonvotingLockedGold(value)
       .runChecks()
-    const election = await getElectionContract(client as WalletClient)
-    const tx = await election.simulate.vote([res.flags.for, value])
-    await displaySendViemContractCall('Election', tx, client)
+
+    const election = await kit.contracts.getElection()
+    const tx = await election.vote(res.flags.for, value)
+    await displaySendTx('vote', tx)
   }
 }
