01ff0f8e99cfaedb68c7f8ae1e936231af1bdbd4
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-05-19 17:18:32 +0200

    wip path configs

--- packages/sdk/contractkit/jest.config.js
+++ packages/sdk/contractkit/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).ts'],
-  setupFilesAfterEnv: ['@celo/dev-utils/lib/matchers', '<rootDir>/jest_setup.ts'],
+  setupFilesAfterEnv: ['@celo/dev-utils/matchers', '<rootDir>/jest_setup.ts'],
   globalSetup: '<rootDir>/src/test-utils/setup.global.ts',
   globalTeardown: '<rootDir>/src/test-utils/teardown.global.ts',
   testSequencer: '<rootDir>/src/test-utils/AlphabeticSequencer.js',
--- packages/sdk/metadata-claims/jest.config.js
+++ packages/sdk/metadata-claims/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/src/**/?(*.)test.ts'],
-  setupFilesAfterEnv: ['@celo/dev-utils/lib/matchers', '<rootDir>/jestSetup.ts'],
+  setupFilesAfterEnv: ['@celo/dev-utils/matchers', '<rootDir>/jestSetup.ts'],
   // globalSetup: '<rootDir>/src/test-utils/setup.global.ts',
   // globalTeardown: '<rootDir>/src/test-utils/teardown.global.ts',
   verbose: true,
