fb1e845305532d3f92158b64841773b42b81e324
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-05-20 16:05:04 +0200

    remove useless comments

--- packages/sdk/governance/src/test-utils/setup.global.ts
+++ packages/sdk/governance/src/test-utils/setup.global.ts
@@ -1,5 +1,3 @@
-// Has to import the matchers somewhere so that typescript knows the matchers have been made available
-
 export default async function globalSetup() {
   console.info('Global setup Complete')
 }
