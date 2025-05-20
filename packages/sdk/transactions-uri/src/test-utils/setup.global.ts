d1d4487188178ec269759a1ed9b4eca21747ab4b
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-05-20 10:16:06 +0200

    wip fix tx-uri

--- packages/sdk/transactions-uri/src/test-utils/setup.global.ts
+++ packages/sdk/transactions-uri/src/test-utils/setup.global.ts
@@ -1,6 +1,1 @@
-// Has to import the matchers somewhere so that typescript knows the matchers have been made available
-
-// Warning: There should be an unused import of '@celo/dev-utils/matchers' above.
-// If there is not, then your editor probably deleted it automatically.
-
 export default async function globalSetup() {}
