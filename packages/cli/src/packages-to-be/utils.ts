43e39083da57a8308c5a1498599e706cd1c071da
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-06-05 11:41:15 +0200

    add method to turn very large big int into a decimal number

--- packages/cli/src/packages-to-be/utils.ts
+++ packages/cli/src/packages-to-be/utils.ts
@@ -1,5 +1,6 @@
 import { CeloClient } from '@celo/actions'
 import BigNumber from 'bignumber.js'
+import { formatEther } from 'viem'
 
 export function bigintToBigNumber(value: bigint) {
   return new BigNumber(value.toString())
@@ -7,6 +8,13 @@ export function bigintToBigNumber(value: bigint) {
 
 export function bigNumberToBigInt(value: BigNumber) {
   return BigInt(value.toFixed())
+}
+
+/* 
+  takes a big int that represents a 24 decimal fixed point number and formats it to a string with 2 decimal places.
+*/
+export function formatFixidity(value: bigint): string {
+  return (Number(formatEther(value)) / 1_000_000.0).toPrecision(2)
 }
 
 export async function ethNodeIsSyncing(client: CeloClient) {
