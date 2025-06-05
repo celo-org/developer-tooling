b854b7e39c57312bbf1c6bddf610b3adcff5e1d2
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-06-05 11:41:15 +0200

    add method to turn very large big int into a decimal number

---
+++ packages/cli/src/packages-to-be/utils.test.ts
@@ -0,0 +1,24 @@
+import { formatFixidity } from './utils'
+
+describe('formatFixidity', () => {
+  it('formats 24-decimal fixed point bigints to 2 decimal places', () => {
+    // 1e24 in fixed point (should be 1.00)
+    const one = 10n ** 24n
+    expect(formatFixidity(one)).toBe('1.0')
+
+    // 1.23e24 in fixed point (should be 1.23)
+    const onePoint23 = 123n * 10n ** 22n
+    expect(formatFixidity(onePoint23)).toBe('1.2')
+
+    // 0 in fixed point (should be 0.00)
+    expect(formatFixidity(0n)).toBe('0.0')
+
+    const small = 10n ** 18n
+    expect(formatFixidity(small)).toBe('0.0000010')
+  })
+
+  it('hands naught point N', () => {
+    const lessThan1 = 8n * 10n ** 23n
+    expect(formatFixidity(lessThan1)).toBe('0.80')
+  })
+})
