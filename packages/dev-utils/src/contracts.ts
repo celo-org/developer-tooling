aaf5cdf8269014cd4b746080729839e6512b975a
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-06-27 17:14:51 +0200

    fix: remove as any

--- packages/dev-utils/src/contracts.ts
+++ packages/dev-utils/src/contracts.ts
@@ -2,12 +2,13 @@ import { StrongAddress } from '@celo/base'
 import AttestationsArtifacts from '@celo/celo-devchain/contracts/contracts-0.5/Attestations.json'
 import Web3 from 'web3'
 import { LinkedLibraryAddress } from './anvil-test'
+import type { AbiItem } from 'web3-utils'
 
 export const deployAttestationsContract = async (
   web3: Web3,
   owner: StrongAddress
 ): Promise<StrongAddress> => {
-  const contract = new web3.eth.Contract(AttestationsArtifacts.abi as any)
+  const contract = new web3.eth.Contract(AttestationsArtifacts.abi as AbiItem[])
 
   const deployTx = contract.deploy({
     data: AttestationsArtifacts.bytecode.replace(
