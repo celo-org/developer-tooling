6c09d4b96b18d7ca2b0140bc71fe9e622dda51be
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-12-02 10:25:48 +0100

    docs(changeset): Updates logic for submiting/confirming transaction with multisig to only search thru non executed transactions

---
+++ .changeset/open-donkeys-nail.md
@@ -0,0 +1,6 @@
+---
+'@celo/contractkit': patch
+'@celo/celocli': patch
+---
+
+Updates logic for submiting/confirming transaction with multisig to only search thru non executed transactions
--- packages/sdk/contractkit/src/wrappers/MultiSig.ts
+++ packages/sdk/contractkit/src/wrappers/MultiSig.ts
@@ -39,8 +39,9 @@ export class MultiSigWrapper extends BaseWrapper<MultiSig> {
   async submitOrConfirmTransaction(destination: string, txObject: CeloTxObject<any>, value = '0') {
     const data = stringToSolidityBytes(txObject.encodeABI())
     const transactionCount = await this.contract.methods.getTransactionCount(true, true).call()
-    let transactionId
-    for (transactionId = Number(transactionCount) - 1; transactionId >= 0; transactionId--) {
+    const transactionIds = await this.contract.methods.getTransactionIds(0, transactionCount, true, false).call()
+
+    for (let transactionId of transactionIds) {
       const transaction = await this.contract.methods.transactions(transactionId).call()
       if (
         transaction.data === data &&
